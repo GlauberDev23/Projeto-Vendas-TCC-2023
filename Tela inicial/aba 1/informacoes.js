b6a1b2e3b3c543aa2cdee1a57ed05db10d1dd805
GlauberDev23 <glaubercastilho04@gmail.com>
2023-09-04 00:35:49 -0300

    primeiro modal finalizado

--- Tela inicial/aba 1/informacoes.js
+++ Tela inicial/aba 1/informacoes.js
@@ -1,6 +1,6 @@
 const zed = document.querySelector(".Zed")
 const modal = document.querySelector("dialog")
-const closeModal = document.querySelector("dialog .fechar")
+const closeModal = document.querySelector("dialog .fecharModal")
 
 zed.onclick = function (){
     modal.showModal()
