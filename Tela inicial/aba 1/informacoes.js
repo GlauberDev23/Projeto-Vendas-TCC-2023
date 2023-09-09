025335c3fda7aa44a57f38ede223f4889d0288a7
GlauberDev23 <glaubercastilho04@gmail.com>
2023-09-09 17:01:03 -0300

    finalizado todos os dialog

--- Tela inicial/aba 1/informacoes.js
+++ Tela inicial/aba 1/informacoes.js
@@ -1,19 +1,112 @@
+///////////////////////// OFERTAS DO MÊS
 const zed = document.querySelector(".Zed")
-const ashe = document.querySelector(".Ashe")
 const modalZed = document.querySelector(".dialogZed")
-const modalAshe = document.querySelector(".dialogAshe")
-const closeModalZed = document.querySelector("dialog .fecharModalZed")
-const closeModalAshe = document.querySelector("dialog .fecharModalAshe")
-
+const closeModalZed = document.querySelector(".fecharModalZed")
 zed.onclick = function (){
     modalZed.showModal()
 }
 closeModalZed.onclick = function (){
     modalZed.close()
 }
+
+const ashe = document.querySelector(".Ashe")
+const modalAshe = document.querySelector(".dialogAshe")
+const closeModalAshe = document.querySelector(".fecharModalAshe")
 ashe.onclick = function (){
     modalAshe.showModal()
 }
 closeModalAshe.onclick = function (){
     modalAshe.close()
+}
+
+const thresh = document.querySelector(".Thresh")
+const modalThresh = document.querySelector(".dialogThresh")
+const closeModalThresh = document.querySelector(".fecharModalThresh")
+thresh.onclick = function (){
+    modalThresh.showModal()
+}
+closeModalThresh.onclick = function (){
+    modalThresh.close()
+}
+
+/////////////////////////////ABA NOVOS
+const mousepad = document.querySelector(".Mousepad")
+const modalMousePad = document.querySelector(".dialogMousepad")
+const closeModalMousepad = document.querySelector(".fecharModalMousepad")
+mousepad.onclick = function (){
+    modalMousePad.showModal()
+}
+closeModalMousepad.onclick = function (){
+    modalMousePad.close()
+}
+
+const draven = document.querySelector(".DravenBoneco")
+const modalDraven = document.querySelector(".dialogDraven")
+const closeModalDraven = document.querySelector(".fecharModalDraven")
+draven.onclick = function (){
+    modalDraven.showModal()
+}
+closeModalDraven.onclick = function (){
+    modalDraven.close()
+}
+
+const blitz = document.querySelector(".BlitzcrankBoneco")
+const modalBlitz = document.querySelector(".dialogBlitz")
+const closeModalBlitz = document.querySelector(".fecharModalBlitz")
+blitz.onclick = function (){
+    modalBlitz.showModal()
+}
+closeModalBlitz.onclick = function (){
+    modalBlitz.close()
+}
+
+const yasuo = document.querySelector(".CadernoYasuo")
+const modalYasuo = document.querySelector(".dialogYasuo")
+const closeModalYasuo = document.querySelector(".fecharModalYasuo")
+yasuo.onclick = function (){
+    modalYasuo.showModal()
+}
+closeModalYasuo.onclick = function (){
+    modalYasuo.close()
+}
+
+//////////////////////////////// QUASE ESGOTANDO
+const quadroThresh = document.querySelector(".QuadroThresh")
+const modalQuadroThresh = document.querySelector(".dialogQuadroThresh")
+const closeModalQuadroThresh = document.querySelector(".fecharModalQuadroThresh")
+quadroThresh.onclick = function (){
+    modalQuadroThresh.showModal()
+}
+closeModalQuadroThresh.onclick = function (){
+    modalQuadroThresh.close()
+}
+
+const quadroKindred = document.querySelector(".QuadroKindred")
+const modalQuadroKindred = document.querySelector(".dialogQuadroKindred")
+const fecharModalQuadroKindred = document.querySelector(".fecharModalQuadroKindred")
+quadroKindred.onclick = function (){
+    modalQuadroKindred.showModal()
+}
+fecharModalQuadroKindred.onclick = function (){
+    modalQuadroKindred.close()
+}
+
+const quadroVi = document.querySelector(".QuadroVi")
+const modalQuadroVi = document.querySelector(".dialogQuadroVi")
+const fecharModalQuadroVi = document.querySelector(".fecharModalQuadroVi")
+quadroVi.onclick = function (){
+    modalQuadroVi.showModal()
+}
+fecharModalQuadroVi.onclick = function (){
+    modalQuadroVi.close()
+}
+
+const quadroKassadin = document.querySelector(".QuadroKassadin")
+const modalQuadroKassadin = document.querySelector(".dialogQuadroKassadin")
+const fecharModalQuadroKassadin = document.querySelector(".fecharModalQuadroKassadin")
+quadroKassadin.onclick = function (){
+    modalQuadroKassadin.showModal()
+}
+fecharModalQuadroKassadin.onclick = function (){
+    modalQuadroKassadin.close()
 }
