14583cb935038fa1d8b78a344e24fb7fae7b669c
GlauberDev23 <glaubercastilho04@gmail.com>
2023-09-24 15:48:25 -0300

    finalizando

--- Tela inicial/aba_Login/Login.js
+++ Tela inicial/aba_Login/Login.js
@@ -1,7 +1,10 @@
 const senha = document.querySelector(".senhaEsquecida")
 const abrirModal = document.querySelector(".dialogPai")
-const fecharModal = document.querySelector(".fecharModalSenha")
+const fecharModal = document.querySelector(".fecharModalSenhaEsquecida")
 
 senha.onclick = function (){
     abrirModal.showModal()
+}
+fecharModal.onclick = function (){
+    abrirModal.close()
 }
